806be873f96329bdeb416bc6e5bb197c1a91f6d9
Zane Ashby <zane.a@demonastery.org>
2012-10-28 00:18:53 +1300

    Made docstrings work.

--- docstrings.scm
+++ docstrings.scm
@@ -2,18 +2,19 @@
 ;;; Docstrings support
 ;;;
 
-(define *documentation-hash-table* (make-hash-table))
+(define-for-syntax *documentation-hash-table* (make-hash-table))
 
-(define-syntax (define* form r c)
+(define-syntax (define* form rename compare)
   (let* ((args-form (second form))
          (proc-name (first args-form))
          (doc (third form))
-         (body (drop form 3)))
-    (hash-table-set! *documentation-hash-table* proc-name (list doc body))
-    `(,(r 'define) ,args-form ,@body)))
+         (body (drop form 3))
+         (define% (rename 'define)))
+    (hash-table-set! *documentation-hash-table* proc-name (list doc body)) 
+    `(,define% ,args-form ,@body)))
 
 (define* (help)
   "Print a list of documented procedures."
   (hash-table-walk *documentation-hash-table*
     (lambda (key val)
-      (printf "~a => ~a~n ~a~n~n" key (first val) (second val)))))
+      (printf "~a => ~a~n ~a~n~n" key (first val) (second val))))) 
